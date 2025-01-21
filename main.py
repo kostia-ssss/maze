3699c02f31666d3934c48996fd1e9c3a966234d5
Kostia <kostiantyn.tel@gmail.com>
2025-01-21 20:24:13 +0200

    v6.0

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ background = pygame.transform.scale(background, (wind_w , wind_h))
 
 pygame.mixer.music.load("sound.mp3")
 pygame.mixer.music.set_volume(0.3)
-#pygame.mixer.music.play(-1)
+pygame.mixer.music.play(-1)
 
 class Sprite:
     def __init__(self , x , y , w , h, img):
@@ -53,8 +53,10 @@ class Player(Sprite):
 
 
 class Enemy(Sprite):
-    def __init__(self , x , y , w , h , img , speed, dir):
-        super().__init__(x, y, w, h, img)
+    def __init__(self , x , y , w , h , img1, img2 , speed, dir):
+        super().__init__(x, y, w, h, img1)
+        self.img_r = self.img
+        self.img_l = pygame.transform.scale(img2, (w, h))
         self.speed = speed
         self.dir = dir
     
@@ -62,15 +64,25 @@ class Enemy(Sprite):
         self.rect.x -= self.speed * self.dir
         if any(self.rect.colliderect(obstacle.rect) for obstacle in maze) or self.rect.left <= 0 or self.rect.right >= window.get_width():
             self.dir *= -1
+            if self.dir == 1:
+                self.img = self.img_l
+            if self.dir == -1:
+                self.img = self.img_r
 
 blocks = []
 Dblocks = []
+Coins = []
+enemies = []
+coins = 0
 block_size = 25
 
 block_x = 0
 block_y = 0
 block_img = pygame.image.load("images/wall.png")
 Dblock_img = pygame.image.load("images/danger_wall.png")
+Coin_img = pygame.image.load("images/coin.png")
+enemy_img1 = pygame.image.load("images/zombie.png")
+enemy_img2 = pygame.transform.flip(enemy_img1, True, False)
 
 def otladka():
     print(len(blocks))
@@ -82,26 +94,29 @@ for row in lvl1:
         elif tile == "2":
             skarb = Sprite(block_x, block_y, 30, 30, pygame.image.load("images/netherite.png"))
         elif tile == "3":
-            enemy = Enemy(block_x, block_y, 30, 30, pygame.image.load("images/zombie.png"), 3, 1)
+            enemies.append(Enemy(block_x, block_y, 30, 30, enemy_img1, enemy_img2, 3, 1))
         elif tile == "4":
             Dblocks.append(Sprite(block_x, block_y, block_size, block_size, Dblock_img))
+        elif tile == "5":
+            Coins.append(Sprite(block_x, block_y, block_size, block_size, Coin_img))
         block_x += block_size
     block_x = 0
     block_y += block_size
 
 player = Player(35, 50, 20, 20, pygame.image.load("images/steve.png"), 3)
 
-otladka()
-
 font = pygame.font.SysFont("Comfortaa" , 50)
 font2 = pygame.font.SysFont("Comfortaa" , 20)
+font3 = pygame.font.SysFont("Comfortaa" , 35)
 win = font.render("You win!", True, (0, 100, 0))
 lose = font.render("You lose(", True, (100, 0, 0))
 reset = font.render("Press R to reset", True, (0, 0, 0))
+coins_t = font3.render(str(coins), True, (0, 0, 0))
 
 game = True
 finish = False
 while game:
+    otladka()
     if not finish:
         window.blit(background, (0, 0))
         
@@ -111,31 +126,44 @@ while game:
         for b in Dblocks:
             b.draw()
         
+        for c in Coins:
+            if not player.rect.colliderect(c.rect):    
+                c.draw()
+            else:
+                coins += 1
+                Coins.remove(c)
+                
+        for e in enemies:
+            e.draw()
+            e.move(blocks)
+            
         player.draw()
         skarb.draw()
-        enemy.draw()
         player.move(blocks)
-        enemy.move(blocks)
 
         if player.rect.colliderect(skarb.rect):
             window.blit(win, (200, 200))
             window.blit(reset, (200, 250))
             finish = True
         
-        if player.rect.colliderect(enemy.rect):
+        if any(player.rect.colliderect(e.rect) for e in enemies):
             window.blit(lose, (200, 200))
             window.blit(reset, (200, 250))
             finish = True
            
         if any(player.rect.colliderect(block.rect) for block in Dblocks):
             player = Player(35, 50, 20, 20, pygame.image.load("images/steve.png"), 3)
+        
+        coins_t = font3.render(str(coins), True, (0, 0, 0)) 
+        window.blit(coins_t, (10, 10))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finish = True
             
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_r and finish == True:
             finish = False
+            coins = 0
             player = Player(35, 50, 20, 20, pygame.image.load("images/steve.png"), 3)
     
     pygame.display.update()
